8ef9b2a5ea6e788180d37417fc7b803114aeaa84
k2t1kim <k2t1kim@nate.com>
2021-04-11 16:50:03 +0900

    aster

---
+++ main.js
@@ -0,0 +1,14 @@
+let str = `
+010-1234-5678
+abc@defg.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+aabbcccdddd
+http://localhost:8080
+동해물과_백두산이
+`
+const h = ` the hello world !
+
+`
+// const regexp = new RegExp('the', 'gi')
+console.log(h.replace(/\s/g, ''))
